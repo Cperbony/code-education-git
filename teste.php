a154ce7f42807d1354f8a75ddbc487697737ec58
Claudinei Perboni <cperbony@gmail.com>
2017-08-22 14:53:21 -0300

    Adicionado a Data();

--- teste.php
+++ teste.php
@@ -1,2 +1,7 @@
 <?php echo "Olá Mundo, Este é um Teste com PHP no VIM para o curso de GIT da Code Education" ?>
-<?php phpinfo()?>
+<?php phpinfo();?>
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+$date = date('Y-m-d H:i');
+echo $date;
+?>
